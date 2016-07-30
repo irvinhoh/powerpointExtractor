ba382a28bcd1b46911b551ada2f61dd5cd56c310
Andrew Chellis <Andrew Chellis>
2016-07-29 20:12:04 -0400

    Updated to no longer be hardcoded for files, can now handle any number of ppts that are in the same folder as the py file

--- ExtractTextFromPpt.py
+++ ExtractTextFromPpt.py
@@ -1,20 +1,45 @@
 import os
 import re
 import codecs
+import zipfile
 
 def main():
 	current_path = os.getcwd()
-	current_path += "/Stuff/ppt/slides/"
+	extractZip(current_path)
+	#Loop through all folders that should have powerpoints
+	for element in os.listdir(current_path):
+		try:
+			if((element.find(".")==-1) and (element!="README")):
+				processSlides(current_path+"/"+element+"/ppt/slides/")
+				#File structure is guaranteed as we just generated it, we can hard code the path in
+		except:
+			print("Failed on "+current_path+"/"+element)
+
+# Extracts all powerpoints and places the contents in a folder with the same name
+# Note: Does write over existing folders
+def extractZip(current_path):
 	fileList = os.listdir(current_path)
-	outputFile = open(current_path+"logfile.txt",'w')
+	for element in fileList:
+		if(element.find(".pptx")!=-1):
+			zippedPpt = zipfile.ZipFile(current_path+"/"+element)
+			output_path = current_path +"/"+ element.replace(".pptx","")+"/"
+			zippedPpt.extractall(output_path)
+
+def processSlides(current_path):
+	fileList = os.listdir(current_path)
+	# Splits the file path to write a text file with the powerpoint name to the same directory level as the python file
+	outputFile = open(current_path[:current_path.find("/ppt")]+current_path[current_path.rfind("/")+1:]+".txt",'w')
 	fileList = sortFiles(fileList)
 	for element in fileList:
 		if element.find(".xml")!=-1:
 			outputFile.write(parseFile(element,current_path))
 
+# Returns all text found in the powerpoint slides
 def parseFile(xmlFile,current_path):
 	fileContents= ""
-	parser = re.compile('>([A-z0-9 ,\(\)\-\:\.\n\?\!;]+)<')
+	parser = re.compile('>([A-z0-9 \,\(\)\-\:\.\n\?\!\;\'\"]+)<')
+	# Trying to handle odd cases, add this pattern ?![[A-z]{3,5}\_[\w\d]
+	# (?![[A-z]{3,5}\_[\w\d])
 	regexResults = ""
 	try:
 		parseXML = open(current_path+xmlFile,'rb')
@@ -29,12 +54,17 @@ def parseFile(xmlFile,current_path):
 
 	except Exception as err:
 		print("Oh no! We had an error opening "+xmlFile+" :(")
-		print(err)
-		print(err.with_traceback)
 
 	return regexResults	
 
+# Takes in list of slides
+# Returns naturally sorted list of slides
 def sortFiles(fileList):
+	# The very, very lazy mans sort:
+	# The list has them slide10, slide 11, ..., slide 1
+	# We want a more natural sort to maintain slide order
+	# We achieve this by parsing each slide name down to its #, then sorting before adding context back
+	# There are far better ways to do it, but this is easy 
 	length = len(fileList)
 	i=0
 	while i<length:
